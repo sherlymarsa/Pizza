0ff961e24eb624c05b68d461dbc670bc22b2ed08
amikom <marsarenaaa@email.com>
2023-11-08 17:05:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pizzaapp/LoginActivity.kt
@@ -0,0 +1,46 @@
+package com.example.pizzaapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //hide title bar
+        getSupportActionBar()?.hide()
+
+        //instance text
+        val txtUsername:EditText = findViewById(R.id.editTextEmail)
+        val txtPassword:EditText = findViewById(R.id.editTextPassword)
+        //instance button login
+        val btnLogin:Button = findViewById(R.id.buttonLogin)
+
+        //event button login
+        btnLogin.setOnClickListener {
+            //object class databaseHelper
+            val databaseHelper = DatabaseHelper(this)
+
+            val email = txtUsername.text.toString().trim()
+            val password = txtPassword.text.toString().trim()
+
+            //check login
+            val result:Boolean = databaseHelper.checkLogin(email,password)
+            if (result == true) {
+                Toast.makeText(this@LoginActivity, "Login Succes ",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intentLogin = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intentLogin)
+            }else{
+                Toast.makeText(this@LoginActivity,"Login Failed, Try Again !!!",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
